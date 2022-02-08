fed2def83bac310a406bb597e201b245bf1b624b
Cat L <97482464+CattL@users.noreply.github.com>
2022-02-07 17:17:59 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+led.enable(false)
+pins.analogWritePin(AnalogPin.P2, 1023)
+pins.analogWritePin(AnalogPin.P1, 1023)
+pins.analogWritePin(AnalogPin.P0, 1023)
+basic.forever(function () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    basic.pause(1000)
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    basic.pause(1000)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P2, 0)
+    basic.pause(1000)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+    for (let index = 0; index < 1; index++) {
+        for (let val = 0; val <= 513; val++) {
+            pins.analogWritePin(AnalogPin.P0, val)
+            pins.analogWritePin(AnalogPin.P1, 1023 - val)
+            pins.analogWritePin(AnalogPin.P2, 512 - val)
+            basic.pause(1000)
+        }
+        for (let val = 0; val <= 513; val++) {
+            pins.analogWritePin(AnalogPin.P0, 512 - val)
+            pins.analogWritePin(AnalogPin.P1, 1023 - val)
+            pins.analogWritePin(AnalogPin.P2, val)
+            basic.pause(1000)
+        }
+    }
+    pins.analogWritePin(AnalogPin.P2, 1023)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P0, 1023)
+})
